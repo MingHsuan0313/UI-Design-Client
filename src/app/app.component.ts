3ae014da14a84f29feb501c2487ad3629ba907f4
SE2019Group1 <william@lijunweide-MacBook-Pro.local>
2020-07-11 12:11:17 +0800

    add UIComponent property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {UIComponent, Icon} from './model';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,9 @@ export class AppComponent implements AfterViewInit {
 
       const vertex1 = graph.insertVertex(parent, '1', 'Vertex 1', 0, 0, 200, 80);
       const vertex2 = graph.insertVertex(parent, '2', 'Vertex 2', 0, 0, 200, 80);
-
+      var c:UIComponent=new Icon();
+      (<Icon>c).text = "1";
+      console.log((<Icon>c).text);
       graph.insertEdge(parent, '', '', vertex1, vertex2);
     } finally {
       graph.getModel().endUpdate();
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,40 +1,152 @@
-export interface UIComponent{
-  x:String;
-  y:String;
-  width:String;
-  height:String;
-  ID:String;
-  selector:String;
+export interface UIComponent {
+  x: String;
+  y: String;
+  width: String;
+  height: String;
+  ID: String;
+  selector: String;
 
-  add(component:UIComponent):void;
-  remove(component:UIComponent):void;
-  getInfo():String;
+  add(component: UIComponent): void;
+  remove(component: UIComponent): void;
+  getInfo(): String;
 }
 
-export class Icon implements UIComponent{
+export class Icon implements UIComponent {
+  x: String;
+  y: String;
+  width: String;
+  height: String;
+  ID: String;
+  selector: String;
+  text: String;
 
-}
+  add(component: UIComponent): void {
+  }
 
-export class Text implements UIComponent{
+  getInfo(): String {
+    return '';
+  }
 
-}
-
-export class Button implements UIComponent{
-
-}
-
-export class Table implements UIComponent{
-
-}
-
-export class CardComposite implements UIComponent{
-
-}
-
-export class DropdownComposite implements UIComponent{
+  remove(component: UIComponent): void {
+  }
 
 }
 
-export class InputGroupComposite implements UIComponent{
+export class Text implements UIComponent {
+  ID: String;
+  height: String;
+  selector: String;
+  width: String;
+  x: String;
+  y: String;
+
+  add(component: UIComponent): void {
+  }
+
+  getInfo(): String {
+    return '';
+  }
+
+  remove(component: UIComponent): void {
+  }
+}
+
+export class Button implements UIComponent {
+  ID: String;
+  height: String;
+  selector: String;
+  width: String;
+  x: String;
+  y: String;
+
+  add(component: UIComponent): void {
+  }
+
+  getInfo(): String {
+    return '';
+  }
+
+  remove(component: UIComponent): void {
+  }
+
+}
+
+export class Table implements UIComponent {
+  ID: String;
+  height: String;
+  selector: String;
+  width: String;
+  x: String;
+  y: String;
+
+  add(component: UIComponent): void {
+  }
+
+  getInfo(): String {
+    return '';
+  }
+
+  remove(component: UIComponent): void {
+  }
+
+}
+
+export class CardComposite implements UIComponent {
+  ID: String;
+  height: String;
+  selector: String;
+  width: String;
+  x: String;
+  y: String;
+
+  add(component: UIComponent): void {
+  }
+
+  getInfo(): String {
+    return '';
+  }
+
+  remove(component: UIComponent): void {
+  }
+
+}
+
+export class DropdownComposite implements UIComponent {
+  ID: String;
+  height: String;
+  selector: String;
+  width: String;
+  x: String;
+  y: String;
+
+  add(component: UIComponent): void {
+  }
+
+  getInfo(): String {
+    return '';
+  }
+
+  remove(component: UIComponent): void {
+  }
+
+}
+
+export class InputGroupComposite implements UIComponent {
+  ID: String;
+  height: String;
+  selector: String;
+  width: String;
+  x: String;
+  y: String;
+
+  add(component: UIComponent): void {
+  }
+
+  getInfo(): String {
+    return '';
+  }
+
+  remove(component: UIComponent): void {
+  }
 
 }
