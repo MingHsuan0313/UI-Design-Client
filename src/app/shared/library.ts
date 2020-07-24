7f7eccfbae509e53b2b8b24edd8ba088badb7f42
jb010506 <jb010506@gmail.com>
2020-07-25 01:54:50 +0800

    hot fix

--- src/app/shared/library.ts
+++ src/app/shared/library.ts
@@ -45,7 +45,7 @@ export const Library = {
     "card": ["text", "dropdown", "button", "table"],
     "breadcrumb": ["text"],
     "inputgroup": ["text", "button", "icon", "dropdown"],
-    "form": ["text", "button", "icon"],
+    "form": ["text", "button", "input"],
   },
 
   "componentValue": {
