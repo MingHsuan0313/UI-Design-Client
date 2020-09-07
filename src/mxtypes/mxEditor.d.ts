6748095fae8846a5fd9a9f7b52eb61cbb90357f0
Tim Hsieh <timhost1233@gmail.com>
2020-09-07 22:52:53 +0800

    add mxEditor.d.ts

--- src/mxtypes/mxEditor.d.ts
+++ src/mxtypes/mxEditor.d.ts
@@ -1,4 +1,13 @@
 declare class mxEditor {
+    graph: any;
+    keyHandler: any;
+    constructor(config);
     constructor();
     addAction(actionname,funct);
+    addVertex(parent,vertex,x,y);
+    redo();
+    undo();
+    save(url,linefeed);
+    setGraphContainer(container);
+    swapStyles(first,second)
 }
