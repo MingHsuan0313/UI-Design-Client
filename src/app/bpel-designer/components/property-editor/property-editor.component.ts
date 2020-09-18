ccb4d1193c93f0658e42420536fc4b5dd376a086
benlight7 <ben1017996@gmail.com>
2020-09-18 22:07:33 +0800

    Fix not render components together
    REASON: throw Error when ngOnInit()

--- src/app/bpel-designer/components/property-editor/property-editor.component.ts
+++ src/app/bpel-designer/components/property-editor/property-editor.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PropertyEditorComponent implements OnInit {
     ngOnInit(): void {
-        throw new Error("Method not implemented.");
     }
 
 }
