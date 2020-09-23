b050860d8986f9fd7a7d4f00a5efce4c0b5a749b
benlight7 <ben1017996@gmail.com>
2020-09-23 18:17:53 +0800

    Add <receive> and <reply>

---
+++ src/app/bpel-designer/models/components/element/basic-activities/reply-element.model.ts
@@ -0,0 +1,12 @@
+import { BPELComponent } from "../../component/BPELComponent.model";
+import { BPELComponentElement } from "../BPELComponent-element.model";
+
+export class ReplyElement implements BPELComponentElement {
+    //TODO:
+    add(component: BPELComponent): void {
+        throw new Error("Method not implemented.");
+    }
+    remove(component: BPELComponent): void {
+        throw new Error("Method not implemented.");
+    }
+}
---
+++ src/app/bpel-designer/models/createBPELComponentStrategy/ReplyStrategy.ts
@@ -0,0 +1,58 @@
+import { GraphStorage, StyleStorage } from "src/app/models/modelDependency";
+import { BPELComponent } from "../components/component/BPELComponent.model";
+import { ICreateBPELComponentStrategy } from "./ICreateBPELComponentStrategy";
+
+export class ReplyStrategy implements ICreateBPELComponentStrategy {
+    strategyName: string = "<reply> strategy";
+    basex: number;
+    basey: number;
+
+    constructor(basex?: number, basey?: number) {
+      if (basex == undefined || basey == undefined) {
+        this.basex = 0;
+        this.basey = 0;
+      } else {
+        this.basex = basex;
+        this.basey = basey;
+      }
+    }
+
+    createReplyVertex(graphStorage: GraphStorage, component: BPELComponent, parent: any) {
+        // image style
+        var style = graphStorage.getGraph().getStylesheet().getDefaultVertexStyle();
+        style[mxConstants.STYLE_PERIMETER] = mxConstants.SHAPE_RECTANGLE;
+        style[mxConstants.STYLE_SHAPE] = mxConstants.STYLE_IMAGE;
+        style[mxConstants.STYLE_IMAGE] = 'src/app/bpel-designer/resources/svg/node.action.reply.svg';
+        style[mxConstants.STYLE_IMAGE_WIDTH] = 300;
+        style[mxConstants.STYLE_IMAGE_HEIGHT] = 300;
+        // font
+        // style[mxConstants.STYLE_FONTFAMILY] = 'Arial';
+        style[mxConstants.STYLE_FONTSIZE] = 30;
+        style[mxConstants.STYLE_FONTCOLOR] = '#000000';
+        style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_LEFT;
+        style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_BOTTOM;
+        style[mxConstants.STYLE_IMAGE_ASPECT] = 0;
+        console.log(style);
+        const styleName = "style" + component.id;
+        const styleStorage = new StyleStorage(styleName, style);
+        graphStorage.getGraph().getStylesheet().putCellStyle(styleName, style);
+
+        const width = 100;
+        const height = 100;
+        let replyGeometry = new mxGeometry(this.basex, this.basey, width, height);
+        let replyVertexStorage = graphStorage.insertSVGVertex(null, component.id, component, replyGeometry, styleStorage, styleName);
+
+        return replyVertexStorage;
+    }
+
+    createComponent(graphStorage: GraphStorage, component: BPELComponent, parent: any) {
+        let replyVertexStorage = this.createReplyVertex(graphStorage, component, parent);
+
+        component["x"] = replyVertexStorage.getVertexX();
+        component["y"] = replyVertexStorage.getVertexY();
+        component["width"] = replyVertexStorage.getVertexWidth();
+        component["height"] = replyVertexStorage.getVertexHeight();
+
+        return replyVertexStorage;
+    }
+}
