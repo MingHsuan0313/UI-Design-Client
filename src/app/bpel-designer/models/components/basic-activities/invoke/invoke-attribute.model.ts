cdc4bce2b3a0e0c17c52ba3737de8b169bb2efd7
benlight7 <ben1017996@gmail.com>
2020-10-30 18:18:01 +0800

    Remove fakedata attribute

--- src/app/bpel-designer/models/components/basic-activities/invoke/invoke-attribute.model.ts
+++ src/app/bpel-designer/models/components/basic-activities/invoke/invoke-attribute.model.ts
@@ -9,17 +9,5 @@ export class InvokeAttribute extends BPELComponentAttribute {
 
     constructor(name?: string) {
         super(name);
-        // fake data
-        let inputVariable = "Main.startRequest.0";
-        let outputVariable = undefined;
-        let partnerLink = "GetDepartmentMain";
-        let operation = "start";
-        let portType = "startInteractionPort";
-
-        this.inputVariable = inputVariable;
-        this.outputVariable = outputVariable;
-        this.partnerLink = partnerLink;
-        this.operation = operation;
-        this.portType = portType;
     }
 }
