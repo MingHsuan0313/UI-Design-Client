6e65d394e6ab14306ce9ad4dd80871e60cae9d06
Tim Hsieh <timhost1233@gmail.com>
2020-11-07 19:15:50 +0800

    add wizard task action

--- src/app/models/store/actions/pipelineTaskAction/pipelineTask.action.ts
+++ src/app/models/store/actions/pipelineTaskAction/pipelineTask.action.ts
@@ -2,29 +2,40 @@ import { Action } from '@ngrx/store';
 import { PipelineStorage,Task,Operation } from '../../../wizard-task-dependency';
 
 export enum PipelineActionTypes {
-
+    CREATE_TASK_ACTION = "[Pipeline] Create Task",
+    CREATE_OPERATION_ACTION = "[Pipeline] Create Task",
+    READ_TASKS_ACTION = "[Pipeline] Read Tasks",
+    READ_OPERATIONPOOL_ACTION =  "[Pipeline] Read Operations",
+    DELETE_TASKS_ACTION = "[Pipeline] Delete Tasks"
 }
 
-export class PipelineCreateTaskAction {
-
+export class PipelineCreateTaskAction implements Action {
+    public type = PipelineActionTypes['CREATE_TASK_ACTION'];
+    constructor(public task: Task) {}
 }
 
 export class PipelineCreateOperationAction {
-
+    public type = PipelineActionTypes['CREATE_OPERATION_ACTION'];
+    constructor(public operation: Operation) {}
 }
 
 export class PipelineReadTasksAction {
+    public type = PipelineActionTypes['READ_TASKS_ACTION'];
 
 }
 
 export class PipelineReadOperationPoolAction {
+    public type = PipelineActionTypes['READ_OPERATIONPOOL_ACTION'];
 
 }
 
 export class PipelineDeleteTaskAction {
+    public type = PipelineActionTypes['DELETE_TASK_ACTION'];
+
 
 }
 
 export class PipelineDeleteTasksAction {
+    public type = PipelineActionTypes['DELETE_TASKS_ACTION'];
 
 }
